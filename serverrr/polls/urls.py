c560c14940793a5e656c81c3fe95cb13fc4258e0
andriy137 <118632353+Andriy137@users.noreply.github.com>
2025-04-15 11:15:03 +0300

    Розробив та впровадив систему реєстрації користувачів у веб додатку

--- serverrr/polls/urls.py
+++ serverrr/polls/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
 
-from polls.views import question_list
+from polls.views import question_list, register_view
 
 app_name = 'polls'
 
 urlpatterns = [
     path('', question_list, name='question_list'),
+    path('register/', register_view, name='register/' )
 ]
--- serverrr/polls/views.py
+++ serverrr/polls/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from .forms import RegistrationForm
 from .models import Question
 
 def question_list(request):
@@ -6,3 +8,19 @@ def question_list(request):
     context = {'questions': questions}
     return render(request, 'question_list.html', context)
 
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('polls:question_list')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'register.html', {'form': form})
